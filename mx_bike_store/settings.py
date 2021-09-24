1d56f4fa005f5d439054eb870ddbf1f02fff7fdb
Michal Misza <miszamm@yahoo.ie>
2021-09-24 14:57:17 +0000

    changes to settings.py file - acces key id and secret acces key

--- mx_bike_store/settings.py
+++ mx_bike_store/settings.py
@@ -183,8 +183,8 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-AWS_ACCESS_KEY_ID =  'AKIA6CKQZJDQNUEQOK7V'  #  os.environ.get('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = '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'  #  os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = 'mx-store'
 
 if development:
